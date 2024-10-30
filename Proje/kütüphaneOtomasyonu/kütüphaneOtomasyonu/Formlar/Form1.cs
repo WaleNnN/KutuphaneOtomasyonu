4055d3aef84c05a6e7841158ed71ad65fe1192a4
Mehmet Emin <meemin313@gmail.com>
2024-10-30 22:00:15 +0300

    Kullanıcı şifre
    kullanıcı şifre ekranının tasarımı yapıldı ve kodları yazıldı.

---
+++ Proje/kütüphaneOtomasyonu/kütüphaneOtomasyonu/Formlar/Form1.cs
@@ -0,0 +1,43 @@
+﻿using kütüphaneOtomasyonu.Formlar;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kütüphaneOtomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kuladi = textBox1.Text;
+            string sifre = textBox2.Text;
+            if(kuladi == "admin" &&  sifre == "admin")
+            {
+                formAnasayfa frmanasayfa = new formAnasayfa();
+                frmanasayfa.Show();
+                this.Hide();
+            }
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
