ae36c67d731a400b7cd1bfef437c4e44ea88cce0
Mehmet Emin <meemin313@gmail.com>
2024-12-25 22:14:44 +0300

    Üye ekleme işlemi
    Girilen bilgiler veritabanındaki tabloya gönderiliyor.

--- kütüphaneOtomasyonu/Formlar/formAnasayfa.cs
+++ kütüphaneOtomasyonu/Formlar/formAnasayfa.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace kütüphaneOtomasyonu.Formlar
-{
+{// githuba proje göndermek için kullandım 
     public partial class formAnasayfa : Form
     {
         public formAnasayfa()
